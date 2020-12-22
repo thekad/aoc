9b71ea2d1fa054ccbb4f65e94bf15ff7010b3e12
Jorge Gallegos <kad@blegh.net>
2020-12-21 22:18:13 -0600

    Day 9 part 2

--- src/nine.rs
+++ src/nine.rs
@@ -5,22 +5,48 @@ use std::path::PathBuf;
 
 pub fn cmd(path: PathBuf) -> Result<(), ParseIntError> {
     if let Ok(lines) = crate::io::lines(path) {
-        let mut numbers: VecDeque<u64> = VecDeque::new();
-        for line in lines {
-            let number: u64 = line.parse().unwrap();
+        let numbers: Vec<u64> = lines.iter().map(|x| x.parse().unwrap()).collect();
+        let mut slice: VecDeque<u64> = VecDeque::new();
+        let mut idx: usize = 0;
+        let mut suspect = 0;
+        for number in &numbers {
             // preamble
-            if numbers.len() <= 25 {
-                numbers.push_back(number);
+            if slice.len() <= 25 {
+                slice.push_back(*number);
                 continue;
             } else {
-                numbers.pop_front();
+                slice.pop_front();
             }
-            let sums = calculate_sums(&numbers);
-            if !sums.contains(&number) {
-                println!("{} is not a sum of any of the previous numbers", number);
+            let sums = calculate_sums(&slice);
+            if !sums.contains(number) {
+                println!(
+                    "{} (at {}) is not a sum of any of the previous 25 numbers",
+                    number, idx
+                );
+                suspect = number.clone();
                 break;
             }
-            numbers.push_back(number);
+            slice.push_back(*number);
+            idx += 1;
+        }
+
+        let mut found = false;
+        for length in 2..=idx {
+            for offset in 0..idx {
+                let slice = &numbers[offset..(offset + length)];
+                let sum: u64 = slice.iter().sum();
+                if sum == suspect {
+                    println!("found contiguous range to sum {}: {:?}", sum, slice);
+                    let min = slice.iter().min().unwrap();
+                    let max = slice.iter().max().unwrap();
+                    println!("min {} + max {} = {}", min, max, *min + *max);
+                    found = true;
+                    break;
+                }
+            }
+            if found {
+                break;
+            }
         }
     }
 
