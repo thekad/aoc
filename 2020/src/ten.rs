00334d624510fcefcb6be2770b81d40af7297a74
Jorge Gallegos <kad@blegh.net>
2021-12-08 12:23:04 -0600

    Moved to subdir

--- 2020/src/ten.rs
+++ 2020/src/ten.rs
@@ -1,5 +1,6 @@
 use std::num::ParseIntError;
 use std::path::PathBuf;
+use itertools::Itertools;
 
 pub fn cmd(path: PathBuf) -> Result<(), ParseIntError> {
     if let Ok(lines) = crate::io::lines(path) {
@@ -8,19 +9,12 @@ pub fn cmd(path: PathBuf) -> Result<(), ParseIntError> {
         let mut next: u8 = 0;
         loop {
             if let Some(found) = find_next_best_adapter(next, &adapters) {
-                println!(
-                    "Next adapter from {} is {} (difference of {})",
-                    next,
-                    found,
-                    found - next,
-                );
                 differences.push(found - next);
                 next = found;
             } else {
                 break;
             }
         }
-        println!("Last adapter is {} (difference of 3)", next + 3);
         let diff1: Vec<u8> = differences.iter().map(|x| *x).filter(|x| *x == 1).collect();
         let mut diff3: Vec<u8> = differences.iter().map(|x| *x).filter(|x| *x == 3).collect();
         // account for the last adapter which is always a diff of 3
@@ -31,12 +25,30 @@ pub fn cmd(path: PathBuf) -> Result<(), ParseIntError> {
             diff3.len(),
             diff1.len() * diff3.len()
         );
+
+        find_all_adapters(&adapters);
     }
 
     Ok(())
 }
 
 fn find_next_best_adapter(joltage: u8, adapters: &Vec<u8>) -> Option<u8> {
+    let possible = find_next_adapters(joltage, adapters);
+
+    if possible.len() > 0 {
+        return Some(*possible.iter().min().unwrap());
+    }
+
+    None
+}
+
+fn find_all_adapters(adapters: &Vec<u8>) {
+    for permutation in adapters.iter().permutations(adapters.len()).unique(){
+        println!("{:?}", permutation);
+    }
+}
+
+fn find_next_adapters(joltage: u8, adapters: &Vec<u8>) -> Vec<u8> {
     let possible: Vec<u8> = adapters
         .iter()
         .map(|x| *x)
@@ -44,9 +56,5 @@ fn find_next_best_adapter(joltage: u8, adapters: &Vec<u8>) -> Option<u8> {
         .filter(|x| x - joltage <= 3)
         .collect();
 
-    if possible.len() > 0 {
-        return Some(*possible.iter().min().unwrap());
-    }
-
-    None
+    possible
 }
