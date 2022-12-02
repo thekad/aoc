7c7cbdd19337b3686b6e1e6f9f3ed9c9e04ebe7b
Jorge Gallegos <kad@blegh.net>
2022-12-02 09:39:14 -0600

    Ground work and First day of AOC 2022

---
+++ 2022/cmd/one.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"sort"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+var oneCmd = &cobra.Command{
+	Use:   "one",
+	Short: "Run the first day's exercises",
+	Run: func(cmd *cobra.Command, args []string) {
+		run(args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(oneCmd)
+}
+
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
+
+func run(args []string) {
+	var filePath string
+	var err error
+
+	if len(args) == 0 {
+		filePath, err = dataPath("day-1.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		filePath, err = filepath.Abs(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	log.Println(fmt.Sprintf("Loading file %s", filePath))
+	chunks, err := readChunks(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	calories := []int{}
+
+	for _, chunk := range chunks {
+		var calorie int
+		for _, c := range chunk {
+			i, _ := strconv.Atoi(c)
+			calorie += i
+		}
+
+		calories = append(calories, calorie)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(calories)))
+
+	fmt.Println("Most calories is", calories[0])
+	fmt.Println("Top 3 calories are", calories[0:3])
+	fmt.Println("Sum of top 3 is", sum(calories[0:3]))
+}
---
+++ 2022/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/thekad/aoc/2022/cmd"
+
+func main() {
+	cmd.Execute()
+}
